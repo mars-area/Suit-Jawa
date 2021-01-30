670f59c5b2c60cfec0eb82eb8c2b839f83e1a890
Mars Area <68634487+mars-area@users.noreply.github.com>
2021-01-30 22:17:59 +0800

    Add files via upload

---
+++ suitjawa.js
@@ -0,0 +1,35 @@
+var tanya = true;
+while( tanya ){
+    // menangkap pilihan user
+    var p = prompt( 'pilih : gajah, semut, orang' );
+    // menangkap pilihan computer
+    // bangkitkan bilangan random
+    var comp = Math.random();
+    if( comp < 0.34) {
+        comp = 'gajah';
+    } else if( comp >= 0.34 && comp < 0.67 ) {
+        comp = 'orang';
+    } else {
+        comp = 'semut';
+    }
+    // menentukan rules
+    var hasil = '';
+    if( p == comp ) {
+        hasil = 'SERI';
+        alert('SERI!');
+    } else if( p == 'gajah' ) {
+        hasil = ( comp == 'orang' ) ? 'MENANG' : 'KALAH';
+    } else if( p == 'orang' ) {
+        hasil = ( comp == 'gajah' ) ? 'KALAH' : 'MENANG';
+    } else if( p == 'semut' ) {
+        hasil = ( comp == 'orang' ) ? 'KALAH' : 'MENANG';
+    } else {
+        hasil = 'Memasukan pilihan yang salah';
+    }
+    // tampilkan hasil
+    alert('Kamu memilih : ' + p + ' \nKomputer memilih : ' + comp + '\n\nMaka hasilnya : Kamu ' + hasil );
+
+    tanya = confirm('lagi?');
+}
+
+alert('Terima Kasih sudah bermain');
